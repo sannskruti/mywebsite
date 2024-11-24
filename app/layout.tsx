2e6cd16c083322261ff61fa596e09efbc27d3583
Sanskrutii03 <manoria.s@northeastern.edu>
2024-11-24 15:07:25 -0500

    v3

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,8 @@ export default function RootLayout({
     <html lang="en" className="!scroll-smooth">
       <head>
         <link rel="icon" href="/favicon.ico" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
       </head>
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
