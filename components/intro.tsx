2c92d1424be64d8bc8591cb900370435e59f7be7
Sanskrutii03 <manoria.s@northeastern.edu>
2024-11-27 19:34:17 -0500

    portfolio v6

--- components/intro.tsx
+++ components/intro.tsx
@@ -64,21 +64,18 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
-
       <motion.h1
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I'm Sanskruti Manoria.</span> I'm a{" "}
-        <span className="font-bold">Software Engineer</span> with{" "}
-        <span className="font-bold">5 years</span> of professional experience. I
-        enjoy building, deploying and maintaining{" "}
+        <span className="font-bold">Software Engineer</span> with professional
+        experience in building, deploying and maintaining{" "}
         <span className="italic">websites & apps</span>. My focus is{" "}
         <span className="underline"> Application Development</span> and{" "}
         <span className="underline"> DevOps</span>
       </motion.h1>
-
       <motion.div
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
         initial={{ opacity: 0, y: 100 }}
